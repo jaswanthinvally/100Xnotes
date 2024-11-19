602529ab3b98b9273d0cd3e932e88ce037442002
jaswanthinvally <jaswanthvenkat001@gmail.com>
2024-11-19 17:35:17 +0530

    routes nand pages added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
-export default  function App () {
-  return <div>
+import { BrowserRouter,Routes,Route} from 'react-router-dom'
+import Dashboard from './pages/Dashboard'
+import Welcome from './pages/Welcome'
+import Signup from './pages/Signup'
+import Signin from './pages/Signin'
 
-  </div>
+export default  function App () {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path='/api/v1/dashboard' element={<Dashboard/>}/>
+      <Route path='/api/v1/welcome' element={<Welcome/>}/>
+      <Route path='/api/v1/signup' element={<Signup/>}/>
+      <Route path='/api/v1/signin' element={<Signin/>}/>
+    </Routes>
+    
+    </BrowserRouter>
+  )
 }
